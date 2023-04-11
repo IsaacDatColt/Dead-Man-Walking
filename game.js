2303b0b7362ebbb705d845f0dd4997bbc91dfb84
Isaac Guzman <isaacdatcolt@Isaacs-MacBook-Pro.local>
2023-04-10 21:29:13 -0500

    Add: cleaned up code and added different zombies to spawn from different locations

--- game.js
+++ game.js
@@ -8,45 +8,56 @@ const zomb = document.getElementById('zomb');
 const surv = document.getElementById('surv');
 let survivor;
 let zombie;
+// let zombie2;
+let zombie2;
 let numZombies = 0;
+let numZombies2 = 0;
 const zombies = [];
+const zombies2 = [];
 let runGame;
 let survivors;
 let zombiesInterval;
+
+
+
 const startButton = document.getElementById('startButton');
 
 
 
 
 
-console.log(zomb);
+
+// console.log(zomb);
 
 
 
 // EVENT LISTENERS 
-window.addEventListener('DOMContentLoaded', function() {
-  survivor = new Runner(400, 340, surv, 50, 50, 60);
-  zombie = new Zombie(canvas.width / 2, 0, zomb, 40, 40, 800);
-
+window.addEventListener('DOMContentLoaded', function () {
+  // survivor = new Runner(400, 340, surv, 50, 50, 100);
+   
   // run the game loop
-  startButton.addEventListener('click', function() {
-  runGame = setInterval(gameLoop, 100);
+  startButton.addEventListener('click', function () {
+    runGame = setInterval(gameLoop, 100);
+    survivor = new Runner(400, 340, surv, 50, 50, 100);
 
-  // const zombies = setInterval(zombieLoop, zombie.speed);
-  
-  survivors = setInterval(survivorLoop, survivor.speed);
-  zombiesInterval = setInterval(zombieLoop, zombie.speed);
+    // survivors = setInterval(survivorLoop, survivor.speed);
+    // zombiesInterval = setInterval(zombieLoop, 2200);
+    // zombiesInterval2 = setInterval(zombieLoop2, zombie2.speed);
+    // zombie = new Zombie(200, 0, zomb, 40, 40, 1200);
+    // zombie2 = new Zombie2(0, 200, zomb, 40, 40, 1200);
+    
+  })
 
-  
+}
 
-})});
+);
 
 
 
 
 
 // document.addEventListener('keydown', movementHandler); 
-document.addEventListener('keydown', moveSurvivor); 
+document.addEventListener('keydown', moveSurvivor);
 
 //SETUP FOR CANVAS RENDERING 
 // 2D rendering context for canvas element
@@ -61,33 +72,50 @@ game.setAttribute('width', getComputedStyle(canvas)['width']);
 
 class Runner {
   constructor(x, y, image, width, height, speed) {
-      this.x = x;
-      this.y = y;
-      this.image = image;
-      this.width = width;
-      this.height = height;
-      this.alive = true;
-      this.speed = speed;
-
-      this.render = function() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-
-       }
-
-  }
-};
-
-class Zombie {
-  constructor(x, y, image, width, height, speed) {
-    this.x = 380;
-    this.y = 0;
+    this.x = x;
+    this.y = y;
     this.image = image;
     this.width = width;
     this.height = height;
     this.alive = true;
     this.speed = speed;
 
-    this.render = function() {
+    this.render = function () {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+    }
+
+  }
+};
+
+class Zombie {
+  constructor(x, y, image, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.image = image;
+    this.width = width;
+    this.height = height;
+    this.alive = true;
+    this.speed = speed;
+
+    this.render = function () {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+    }
+  }
+}
+
+class Zombie2 {
+  constructor(x, y, image, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.image = image;
+    this.width = width;
+    this.height = height;
+    this.alive = true;
+    this.speed = speed;
+
+    this.render = function () {
       ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 
     }
@@ -101,14 +129,14 @@ class Zombie {
 function moveSurvivor(e) {
   console.log('movement :', e.key);
 
-  if (e.key === 'ArrowUp')  {
-      survivor.y >= 10 ? (survivor.y -= 10) : null;
+  if (e.key === 'ArrowUp') {
+    survivor.y >= 10 ? (survivor.y -= 10) : null;
   } else if (e.key === 'ArrowDown') {
-      survivor.y + 10 <= canvas.height - survivor.height ? (survivor.y += 10) : null;
+    survivor.y + 10 <= canvas.height - survivor.height ? (survivor.y += 10) : null;
   } else if (e.key === 'ArrowRight') {
-        survivor.x + 10 <= canvas.width - survivor.width ? (survivor.x += 10) : null;
+    survivor.x + 10 <= canvas.width - survivor.width ? (survivor.x += 10) : null;
   } else if (e.key === 'ArrowLeft') {
-        survivor.x - 10 >= 0 ? (  survivor.x -= 10) : null;
+    survivor.x - 10 >= 0 ? (survivor.x -= 10) : null;
   }
 }
 
@@ -117,20 +145,37 @@ function moveZombie(zombie) {
   const directions = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
   const randomDirection = directions[Math.floor(Math.random() * directions.length)];
   if (randomDirection === 'ArrowDown') {
-    zombie.y + 15 <= canvas.height - zombie.height ? (zombie.y += 5) : null;
+    zombie.y + 15 <= canvas.height - zombie.height ? (zombie.y += 4) : null;
   } else if (randomDirection === 'ArrowRight') {
     zombie.x + 6 <= canvas.width - zombie.width ? (zombie.x += 7) : null;
   } else if (randomDirection === 'ArrowLeft') {
-    zombie.x - 10 >= 0 ? (zombie.x -= 5) : null;
+    zombie.x - 10 >= 0 ? (zombie.x -= 7) : null;
+  } else if (randomDirection === 'ArrowUp') {
+    zombie.y - 10 >= 0 ? (zombie.y -= 2) : null;
+
   }
 }
-
+function moveZombie2(zombie2) {
+  const directions = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+  const randomDirection = directions[Math.floor(Math.random() * directions.length)];
+  if (randomDirection === 'ArrowUp') {
+    zombie2.y - 10 >= 0 ? (zombie2.y -= 5) : null;
+  } else if (randomDirection === 'ArrowDown') {
+    zombie2.y + 15 <= canvas.height - zombie2.height ? (zombie2.y += 5) : null;
+  } else if (randomDirection === 'ArrowRight') {
+    zombie2.x + 6 <= canvas.width - zombie2.width ? (zombie2.x += 7) : null;
+  }
+  // } else if (randomDirection === 'ArrowLeft') {
+  //   zombie2.x - 10 >= 0 ? (zombie2.x -= 5) : null;
+  // }
+}
 
 //GAME Processes  GAMELOOP
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.imageSmoothingEnabled = false;
   
+
   for (let i = 0; i < numZombies; i++) {
     if (checkCollision(survivor, zombies[i])) {
       clearInterval(runGame);
@@ -140,51 +185,90 @@ function gameLoop() {
       break;
     }
   }
-  
+ 
+
   function checkCollision(obj1, obj2) {
     return (
-      obj1.x + 10 < obj2.x + obj2.width &&
-      obj1.x + obj1.width - 30 > obj2.x &&
+      obj1.x + 20 < obj2.x + obj2.width &&
+      obj1.x + obj1.width - 40 > obj2.x &&
       obj1.y + 10 < obj2.y + obj2.height &&
-      obj1.y + obj1.height - 30 > obj2.y
+      obj1.y + obj1.height - 25 > obj2.y
     );
   }
   
 
-    zombieLoop();
-    survivor.render();
+  // zombieLoop2();
+  zombieLoop();
+  survivor.render();
   // zombie.render();
-  
+
 }
 
 
 //Seperate Game processing ==== SURVIVOR LOOP
 function survivorLoop() {
-
   survivor.render();
 }
 
 
 //zombie loop function so that new zombies are pushed in at random every 10 seconds
-function zombieLoop() { {
-    if (Math.random() < 0.001) { // add a new zombie with a 0.001% chance to enter every 10 seconds
-      zombies.push(new Zombie(canvas.width / 2, 0, zomb, 40, 40, 100));
+function zombieLoop() {
+  
+    if (Math.random() < 0.0015) { // add a new zombie with a 0.015% chance to enter every 10 seconds
+      zombies.push(new Zombie(100, 0, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(760, 0, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(660, 0, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(560, 0, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(700, 0, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(760, 130, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(760, 350, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(0, 100, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(0, 260, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(60, 0, zomb, 40, 40, 1200));
+      zombies.push(new Zombie(100, 0, zomb, 40, 40, 1200));
+
+
       numZombies++;
+     
+
     }
-  }
 
   for (let i = 0; i < numZombies; i++) {
     moveZombie(zombies[i]);
     zombies[i].render();
   }
-  
+ 
+  zombieIntervalSpeed = 2200;
 }
+
 // Zombie spawn Timer
 setInterval(() => {
-  if (numZombies < 8) {
-    zombies.push(new Zombie(canvas.width / 2, 0, zomb, 40, 40, 100));
+  if (numZombies < 150) {
+    zombies.push(new Zombie(canvas.width / 2, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(0, 300, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(0, 100, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(100, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(300, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(500, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(400, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(600, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(300, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(160, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(500, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(700, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(260, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(560, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(760, 130, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(760, 230, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(200, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(125, 0, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(0, 150, zomb, 40, 40, 1200));
+    zombies.push(new Zombie(0, 300, zomb, 40, 40, 1200));
+
+
+
+
     numZombies++;
   }
-}, 10000);
-
+}, 1000);
 
